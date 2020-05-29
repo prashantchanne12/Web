2c1ac9fed27e5ae7375bfd13935bdbb1db41329a
prashantchanne12 <prashantchanne121@gmail.com>
2020-05-29 13:18:52 +0530

    Added Loan Calculator -> js

---
+++ js/Loan Calculator/app.js
@@ -0,0 +1,80 @@
+// Listen for submit
+document.querySelector('#loan-form').addEventListener('submit', function (event) {
+    // Hide Results
+    document.querySelector('#results').style.display = 'none';
+
+    // Show Loader
+    document.querySelector('#loading').style.display = 'block';
+
+    setTimeout(calculateResults, 500);
+
+    event.preventDefault();
+});
+
+function calculateResults() {
+
+    // UI Variables
+    const amount = document.querySelector('#amount');
+    const interest = document.querySelector('#interest');
+    const years = document.querySelector('#years');
+    const monthlyPayment = document.querySelector('#monthly-payment');
+    const totalPayment = document.querySelector('#total-payment');
+    const totalInterest = document.querySelector('#total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payments
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        // Show Results
+        document.querySelector('#results').style.display = 'block';
+
+        // Hide spinner
+        document.querySelector('#loading').style.display = 'none';
+
+
+
+    } else {
+
+        // Show Results
+        document.querySelector('#results').style.display = 'none';
+
+        // Hide spinner
+        document.querySelector('#loading').style.display = 'none';
+        // console.log(monthly); -> NaN
+        showError('Please check your numbers');
+    }
+
+}
+
+// Show Error
+function showError(error) {
+    // Create div
+    const errorDiv = document.createElement('div');
+
+    // Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // CLear Error after 3s
+    setTimeout(function () {
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
